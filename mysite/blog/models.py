4138785e14a847d7e22eb9163af7bb43bd981d55
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2020-12-16 12:02:02 +0300

    add_viev_blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -23,6 +24,9 @@ class Post(models.Model):
 
     objects = models.Manager()  # Менеджер по умолчанию.
     published = PublishedManager()  # Наш новый менеджер.
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
