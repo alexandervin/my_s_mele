20b15f10bc49dbd0aac7c28883a0cc7aeeee9521
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-12-20 15:32:50 +0300

    add_comments_in_admin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager): 
     def get_queryset(self): 
-        return super(PublishedManager, self).get_queryset().filter(status='published')
+        return super(models.Manager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model): 
@@ -28,8 +28,8 @@ class Post(models.Model):
                               choices=STATUS_CHOICES, 
                               default='draft') 
     
-    objects = models.Manager() # The default manager. 
-    published = PublishedManager() # Our custom manager.
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     class Meta: 
         ordering = ('-publish',) 
@@ -43,3 +43,20 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+class Meta:
+    ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {} '.format(self.name, self.post)
