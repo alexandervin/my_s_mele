be73ec0c3029bcd21a3f62627e7cf72b9cb54e05
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2021-01-19 09:44:43 +0300

    add_search_in_blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,10 +5,13 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_list(request, tag_slug=None):
-    object_list = Post.published.all()
+    object_list = Post.objects.all()
     tag = None
 
     if tag_slug:
@@ -90,3 +93,25 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
         return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        results = Post.objects.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+        return render(request, 'blog/post/search.html', {'form': form,
+                                                         'query': query,
+                                                         'results': results})
+    search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+    search_query = SearchQuery(query)
+    results = Post.objects.annotate(
+        search=search_vector,
+        rank=SearchRank(search_vector, search_query)
+    ).filter(search=search_query).order_by('-rank')
+
+
